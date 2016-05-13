9d5e7d6c194b737b22fe8c35fce604373b572978
Adam Ross <adamgarrettross@gmail.com>
2016-05-13 11:04:05 -0500

    Group by and count.

---
+++ count_and_group_by.sql
@@ -0,0 +1,17 @@
+#count and group by
+
+-- SELECT count(title)
+-- FROM film
+-- ;
+
+-- SELECT title, rental_rate
+-- FROM film
+-- group by rental_rate
+-- ;
+
+# Using the count and group by in symphony, could also put group by 2 -- 2 is equal to the col #
+
+SELECT count(title), rental_rate
+FROM film
+group by rental_rate
+;
