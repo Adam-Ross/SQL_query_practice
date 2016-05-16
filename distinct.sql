4fbf00fa9979951ac61d02974f061b325bf1c0f4
Adam Ross <adamgarrettross@gmail.com>
2016-05-16 06:49:01 -0500

    Monthly numbers, averages of rentals and customers.

--- distinct.sql
+++ distinct.sql
@@ -1,9 +1,9 @@
 #number of unique renters by month.
 
 SELECT
-    left(r.rental_date, 7) as Month, COUNT(r.rental_id) as "Total rentals", COUNT(DISTINCT r.customer_id) as "Number of customers", COUNT(r.rental_id) / COUNT(DISTINCT r.customer_id) as "Rentals per customer"
+  left(r.rental_date, 7) as Month, COUNT(r.rental_id) as "Monthly Number of total rents", COUNT(distinct r.customer_id) as "Monthly Number of customers", COUNT(r.rental_id) / COUNT(distinct r.customer_id) as "Monthly Average rentals per person"
 FROM
-    rental r
+  rental r
 GROUP BY
-    1
+  1
 ;
