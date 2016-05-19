e8e0e614463683e224257df59ad01c34239e8c07
Adam Ross <adamgarrettross@gmail.com>
2016-05-19 18:32:35 -0500

    Subqueries.

--- subqueries.sql
+++ subqueries.sql
@@ -8,4 +8,13 @@ FROM Movies
 Where id IN
   (SELECT movie_id
   FROM Promotions
-  WHERE category = “non cash”);
+  WHERE category = “non cash”); #---------- Querie surrounded by parens is called a 'subquerie.'
+                                # Subqueries get ran first.
+
+# IN this particular example, could use a join querie.
+
+SELECT SUM(sales)
+FROM Movies m
+INNER JOIN Promotions p
+ON m.id = p.movie_id
+WHERE p.category = "non cash";
